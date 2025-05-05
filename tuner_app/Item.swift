0388f2ee596bb04767bb6c592888a638e4276da2
Yuriy Egorov <yuriy.egorov@team.bumble.com>
2025-05-05 20:19:06 +0100

    init project

---
+++ tuner_app/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  tuner_app
+//
+//  Created by Yuriy Egorov on 5/5/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ tuner_appTests/tuner_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  tuner_appTests.swift
+//  tuner_appTests
+//
+//  Created by Yuriy Egorov on 5/5/25.
+//
+
+import Testing
+@testable import tuner_app
+
+struct tuner_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
